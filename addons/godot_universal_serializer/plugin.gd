daba74b143c0d3ef33b7666e0601baa4238007ee
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2022-11-27 00:39:08 +0800

    Refactored, improve performance. Implement as static class.

--- addons/godot_universal_serializer/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree() -> void:
-	add_autoload_singleton("GUS","res://addons/godot_universal_serializer/GUS.gd")
-
-func _exit_tree() -> void:
-	remove_autoload_singleton("GUS")
