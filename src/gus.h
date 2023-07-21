3389d7175ac6655a3843cf279675f1e077dc389b
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2023-07-22 00:21:59 +0800

    Update expose interface.

--- src/gus.h
+++ src/gus.h
@@ -13,7 +13,6 @@
  *
  * 注意：
  * 	1、不支持与 Object 相关的类型 （Object, RID , Callable, Signal), 容器类型中也不能包含。
- * 	2、暂不支持自行构建 real_t 为 double 的 Godot 4.x 版本(编译出问题)。
  *
  * @copyright Copyright (c) 2022
  *  MIT License
@@ -22,29 +21,22 @@
 #define GODOT_UNIVERSAL_SERIALIZER_H
 
 #include <godot_cpp/classes/object.hpp>
-#include <godot_cpp/core/binder_common.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
-#include <godot_cpp/classes/wrapped.hpp>
-
-// #define REAL_T_IS_DOUBLE
-
 #include "dserializer.gdany.hpp"
-
-/*
-	类型化数组特化
-	对象相关类型
-*/
 
 class GUS : public Object {
 	GDCLASS(GUS, Object)
 protected:
 	static void _bind_methods() {
-		godot::ClassDB::bind_static_method("GUS", D_METHOD("get_color_encode_type"), GUS::get_color_encode_type);
-		godot::ClassDB::bind_static_method("GUS", D_METHOD("set_color_encode_type", "color_encode_type"), GUS::set_color_encode_type, DEFVAL(ColorEncodeType::COLOR_ENCODE_TYPE_RAW));
-		
-		godot::ClassDB::bind_static_method("GUS", D_METHOD("var_to_bytes", "var"), GUS::var_to_bytes);
-		godot::ClassDB::bind_static_method("GUS", D_METHOD("bytes_to_var", "bytes"), GUS::bytes_to_var);
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("get_color_encode_type"), GUS::get_color_encode_type);
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("set_color_encode_type", "color_encode_type"), GUS::set_color_encode_type, DEFVAL(ColorEncodeType::COLOR_ENCODE_TYPE_RAW));
+
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("is_varint_incoding_in_packed_array"), GUS::is_varint_incoding_in_packed_array);
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("set_varint_incoding_in_packed_array", "enabled"), GUS::set_varint_incoding_in_packed_array);
+
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("var_to_bytes", "var"), GUS::var_to_bytes);
+		godot::ClassDB::bind_static_method(get_class_static(), D_METHOD("bytes_to_var", "bytes"), GUS::bytes_to_var);
 
 		BIND_ENUM_CONSTANT(COLOR_ENCODE_TYPE_RAW);
 		BIND_ENUM_CONSTANT(COLOR_ENCODE_TYPE_HEX);
@@ -57,38 +49,32 @@ public:
 		COLOR_ENCODE_TYPE_HEX, // uint32_t
 		COLOR_ENCODE_TYPE_HEX64, // uint64_t
 	};
-	static ColorEncodeType get_color_encode_type(){return _color_encode_type;}
-	static void set_color_encode_type(ColorEncodeType color_encode_type = COLOR_ENCODE_TYPE_HEX) {
-		_color_encode_type = color_encode_type;
-		switch (_color_encode_type) {
+	static ColorEncodeType get_color_encode_type() { return color_encode_type; }
+	static void set_color_encode_type(ColorEncodeType p_color_encode_type = COLOR_ENCODE_TYPE_HEX) {
+		color_encode_type = p_color_encode_type;
+		switch (color_encode_type) {
 			case COLOR_ENCODE_TYPE_RAW: {
 				dserializer::color_encode_code = dserializer::DType::COLOR;
 				dserializer::color_arr_encode_code = dserializer::DType::PACKED_COLOR_ARRAY;
-				dserializer::cal_size_color = &dserializer::cal_size_color_raw<int64_t>;
-				dserializer::encode_color = &dserializer::encode_color_raw;
-				dserializer::cal_size_color_arr = &dserializer::cal_size_color_arr_raw<int64_t>;
-				dserializer::encode_color_arr = &dserializer::encode_color_arr_raw;
 			} break;
 			case COLOR_ENCODE_TYPE_HEX: {
 				dserializer::color_encode_code = dserializer::DType::COLOR_HEX;
 				dserializer::color_arr_encode_code = dserializer::DType::PACKED_COLOR_ARRAY_HEX;
-				dserializer::cal_size_color = &dserializer::cal_size_color_hex<int64_t>;
-				dserializer::encode_color = &dserializer::encode_color_hex;
-				dserializer::cal_size_color_arr = &dserializer::cal_size_color_arr_hex<int64_t>;
-				dserializer::encode_color_arr = &dserializer::encode_color_arr_hex;
 			} break;
 			case COLOR_ENCODE_TYPE_HEX64: {
 				dserializer::color_encode_code = dserializer::DType::COLOR_HEX64;
 				dserializer::color_arr_encode_code = dserializer::DType::PACKED_COLOR_ARRAY_HEX64;
-				dserializer::cal_size_color = &dserializer::cal_size_color_hex64<int64_t>;
-				dserializer::encode_color = &dserializer::encode_color_hex64;
-				dserializer::cal_size_color_arr = &dserializer::cal_size_color_arr_hex64<int64_t>;
-				dserializer::encode_color_arr = &dserializer::encode_color_arr_hex64;
 			} break;
 
 			default:
 				break;
 		}
+	}
+	static void set_varint_incoding_in_packed_array(bool p_enabled) {
+		dserializer::varint_encoding_in_packed_array = p_enabled;
+	}
+	static bool is_varint_incoding_in_packed_array() {
+		return dserializer::varint_encoding_in_packed_array;
 	}
 
 	static godot::PackedByteArray var_to_bytes(const godot::Variant &var) {
@@ -96,7 +82,7 @@ public:
 		decltype(r.size()) size = 0;
 		dserializer::cal_size(var, size);
 		r.resize(size);
-		buffer_ptr buf = r.ptrw();
+		buffer_t *buf = r.ptrw();
 		dserializer::encode(buf, var);
 		return r;
 	}
@@ -109,10 +95,10 @@ public:
 	}
 
 private:
-	static ColorEncodeType _color_encode_type;
+	static ColorEncodeType color_encode_type;
 };
 
-GUS::ColorEncodeType GUS::_color_encode_type = GUS::COLOR_ENCODE_TYPE_RAW;
+GUS::ColorEncodeType GUS::color_encode_type = GUS::COLOR_ENCODE_TYPE_RAW;
 
 VARIANT_ENUM_CAST(GUS::ColorEncodeType);
 
--- src/register_types.cpp
+++ src/register_types.cpp
@@ -30,38 +30,24 @@
 
 #include "register_types.h"
 
-#include <gdextension_interface.h>
-
-#include <godot_cpp/core/class_db.hpp>
-#include <godot_cpp/core/defs.hpp>
-#include <godot_cpp/godot.hpp>
-
-
 #include "gus.h"
 
-using namespace godot;
-
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_gus_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 	ClassDB::register_abstract_class<GUS>();
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
-}
+void uninitialize_gus_module(ModuleInitializationLevel p_level);
 
 extern "C" {
-
 // Initialization.
+GDExtensionBool GDE_EXPORT gus_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-GDExtensionBool GDE_EXPORT gus_library_init(const GDExtensionInterface *p_interface,
-		const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-	// p_interface
-	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
-
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.register_initializer(initialize_gus_module);
+	init_obj.register_terminator(uninitialize_gus_module);
 	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	return init_obj.init();
