5fa00479ddaf782b118bc37b8eae91f6e821c6d6
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2023-07-22 14:14:22 +0800

    Remove last built files before building

--- build_tool.py
+++ build_tool.py
@@ -33,38 +33,46 @@ def main():
         if arg.startswith("target"):
             debug_and_relaese = False
 
-        if debug_and_relaese:
-            print("Building debug version...")
-            os.system(args + " target=template_debug")
+    # Remove all last build files.
+    for f in os.listdir("bin"):
+        for suffix in dynamic_lib_suffixs:
+            if not f.endswith(suffix):
+                continue
+        os.remove(path_join(bin_dir, f))
 
-            print("Building release version...")
-            os.system(args + " target=template_release")
-        else:
-            print("Building...")
-            os.system(args)
-        print("Build finished!")
+    # Buiild.
+    if debug_and_relaese:
+        print("Building debug version...")
+        os.system(args + " target=template_debug")
 
-        # Post process
-        bin_dir = "bin"
-        plugin_dir = "demo/addons/com.daylily_zeleen.godot_universal_serializer"
-        dynamic_lib_suffixs = [".so", ".dylib", ".wasm", ".dll"]
+        print("Building release version...")
+        os.system(args + " target=template_release")
+    else:
+        print("Building...")
+        os.system(args)
+    print("Build finished!")
 
-        # Copy dynamic liberary.
-        dst_dir = path_join(plugin_dir, "bin")
-        for f in os.listdir("bin"):
-            for suffix in dynamic_lib_suffixs:
-                if not f.endswith(suffix):
-                    continue
-                shutil.copyfile(path_join(bin_dir, f), path_join(dst_dir, f))
+    # Post process
+    bin_dir = "bin"
+    plugin_dir = "demo/addons/com.daylily_zeleen.godot_universal_serializer"
+    dynamic_lib_suffixs = [".so", ".dylib", ".wasm", ".dll"]
 
-        # Copy readme and license.
-        shutil.copyfile("README.md", path_join(plugin_dir, "README.md"))
-        shutil.copyfile("README_zh_cn.md", path_join(plugin_dir, "README_zh_cn.md"))
-        shutil.copyfile("LICENSE", path_join(plugin_dir, "LICENSE"))
+    # Copy dynamic liberary.
+    dst_dir = path_join(plugin_dir, "bin")
+    for f in os.listdir("bin"):
+        for suffix in dynamic_lib_suffixs:
+            if not f.endswith(suffix):
+                continue
+            shutil.copyfile(path_join(bin_dir, f), path_join(dst_dir, f))
 
-        shutil.copyfile("README.md", path_join("demo", "README.md"))
-        shutil.copyfile("README_zh_cn.md", path_join("demo", "README_zh_cn.md"))
-        shutil.copyfile("LICENSE", path_join("demo", "LICENSE"))
+    # Copy readme and license.
+    shutil.copyfile("README.md", path_join(plugin_dir, "README.md"))
+    shutil.copyfile("README_zh_cn.md", path_join(plugin_dir, "README_zh_cn.md"))
+    shutil.copyfile("LICENSE", path_join(plugin_dir, "LICENSE"))
+
+    shutil.copyfile("README.md", path_join("demo", "README.md"))
+    shutil.copyfile("README_zh_cn.md", path_join("demo", "README_zh_cn.md"))
+    shutil.copyfile("LICENSE", path_join("demo", "LICENSE"))
 
 
 "com.daylily_zeleen.godot_universal_serializer/bin"
