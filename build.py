6baa03c2d649bd451f1786687e8512fcc59a54b0
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2023-07-30 21:40:20 +0800

    update build system

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ def main():
     if debug_and_relaese:
         print("Building debug version...")
         os.system(args + " target=template_debug")
-
+        print("")
         print("Building release version...")
         os.system(args + " target=template_release")
     else:
@@ -48,29 +48,47 @@ def main():
 
     # == Post process ==
     # Copy dynamic library to dist dir.
+    print("Copy dynamic library to dist dir.")
     dynamic_lib_suffixs = [".so", ".dylib", ".wasm", ".dll"]
     dist_dir = "dist/addons/com.daylily_zeleen.godot_universal_serializer2/"
     dist_bin_dir = path_join(dist_dir, "bin")
 
+    dev_libs = []
     for f in os.listdir("bin/"):
         for suffix in dynamic_lib_suffixs:
             if not f.endswith(suffix):
                 continue
             path = path_join(bin_dir, f)
             dist_path = path_join(dist_bin_dir, f)
+            if f.count(".dev."):
+                dev_libs.append(f)
+            else:
+                if os.path.isfile(path):
+                    shutil.copyfile(path, dist_path)
+                else:
+                    shutil.copytree(path, dist_path)
+
+    # Try dev libs as debug libs.
+    if len(dev_libs) > 0:
+        print("Copy and replace dev libs to debug libs.")
+        for f in dev_libs:
+            path = path_join(bin_dir, f)
+            dist_path = path_join(dist_bin_dir, f).replace(".dev.", ".")
             if os.path.isfile(path):
                 shutil.copyfile(path, dist_path)
             else:
                 shutil.copytree(path, dist_path)
 
     # Copy readme and license to dist path.
+    print("Copy readme and license to dist path.")
     shutil.copyfile("README.md", path_join(dist_dir, "README.md"))
     shutil.copyfile("README_zh_cn.md", path_join(dist_dir, "README_zh_cn.md"))
     shutil.copyfile("LICENSE", path_join(dist_dir, "LICENSE"))
 
     # Copy plugins to demo for test.
+    print("Copy plugins to demo for test.")
     if os.path.exists("demo/addons/"):
-        os.removedirs("demo/addons/")
+        shutil.rmtree("demo/addons/")
     shutil.copytree("dist/addons", "demo/addons")
 
     print("Done!")
